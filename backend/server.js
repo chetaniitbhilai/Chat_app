d5fbfbe32451b605658f2d80c6e1e82257c382f6
chetaniitbhilai <130583211+chetaniitbhilai@users.noreply.github.com>
2024-05-28 09:25:54 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
@@ -15,6 +16,8 @@ dotenv.config(); // Load environment variables from .env file
 
 const PORT = process.env.PORT || 5000; // Use port from env file, else default to 5000
 
+const __dirname=path.resolve()
+
 // Setting up middleware
 app.use(express.json()); // Parse incoming requests with JSON payloads
 app.use(cookieParser()); // Access cookies
@@ -23,6 +26,12 @@ app.use(cookieParser()); // Access cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 // Start the server and connect to MongoDB
 server.listen(PORT, () => {
